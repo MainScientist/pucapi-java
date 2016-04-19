6159665c9569ba08fa76215d50c2514d8d2bb108
MainScientist <fipezoa@uc.cl>
2016-04-19 16:38:00 -0300

    macadresses

---
+++ src/cl/uc/fipezoa/pucapi/portaluc/UCMacAddress.java
@@ -0,0 +1,20 @@
+package cl.uc.fipezoa.pucapi.portaluc;
+
+/**
+ * Created by MainScientist on 4/19/16.
+ */
+public class UCMacAddress {
+
+    public static String SMARTPHONE = "";
+
+
+    public String name;
+    public String address;
+    public String type;
+
+    public UCMacAddress(String address, String type){
+        this.name = name;
+        this.address = address;
+        this.type = type;
+    }
+}
